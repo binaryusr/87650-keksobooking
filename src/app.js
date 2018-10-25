6615dd6a4aba2d679232baec6096be75c7d31731
binaryusr <dm.kolotov@mail.ru>
2018-10-26 00:01:23 +0200

    Removed unnecessary code

--- src/app.js
+++ src/app.js
@@ -9,10 +9,4 @@ app.use(express.static(`${process.cwd()}/static`));
 
 app.use(`/api/offers`, offersRouter);
 
-const NOT_FOUND_HANDLER = (req, res) => {
-  res.status(404).send(`File or page was not found`);
-};
-
-app.use(NOT_FOUND_HANDLER);
-
 module.exports = app;
