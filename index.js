0cd69c2b46367f4e876ecbf3060ad5d7cd7c4fa7
binaryusr <dm.kolotov@mail.ru>
2018-11-05 23:41:47 +0100

    Added environment variables

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+require(`dotenv`).config();
+
+if (process.env.NODE_ENV !== `production`) {
+  console.log(`Database host address:`, process.env.DB_HOST);
+  console.log(`Database username:`, process.env.DB_USER);
+  console.log(`Database password:`, process.env.DB_PASSWORD);
+  console.log(`Server log level:`, process.env.SERVER_LOG_LEVEL);
+}
+
 const execute = require(`./src/execute`);
 const args = process.argv.slice(2);
 
--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@
 
 const {MongoClient} = require(`mongodb`);
 
-const {DbName, MONGO_URL} = require(`./utils/constants`);
+const {DbName} = require(`./utils/constants`);
 
-module.exports = MongoClient.connect(MONGO_URL, {useNewUrlParser: true})
+module.exports = MongoClient.connect(`mongodb://${process.env.DB_HOST}`, {useNewUrlParser: true})
   .then((client) => client.db(DbName.KEKSOBOOKING))
   .catch((err) => {
     console.error(`Failed to connect to MongoDB`, err);
