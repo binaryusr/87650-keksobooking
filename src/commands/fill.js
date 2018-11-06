414c8658c6afde183215a41116ca1ca9be03614d
binaryusr <dm.kolotov@mail.ru>
2018-11-06 16:58:27 +0100

    Implemented fill functionality with test images, still broken

--- src/commands/fill.js
+++ src/commands/fill.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const fs = require(`fs`);
+
 const logger = require(`../logger`);
 
 const generateTestOffersStandard = require(`../../test/data/generate-test-offers-standard`);
 const offersStore = require(`../offers/store`);
+const ImageStore = require(`../images/store`);
 
 module.exports = {
   name: `fill`,
@@ -12,6 +15,16 @@ module.exports = {
     const data = generateTestOffersStandard();
     try {
       await offersStore.saveMany(data);
+      Promise.all(data.map((it) => {
+        const promises = [];
+        if (it.author.avatar) {
+          promises.push(ImageStore.saveAvatar(it.date, fs.createReadStream(`${process.cwd()}/test/fixtures/keks.png`)));
+        }
+        if (it.offer.preview) {
+          promises.push(ImageStore.savePreview(it.date, fs.createReadStream(`${process.cwd()}/test/fixtures/keks.png`)));
+        }
+        return Promise.all(promises);
+      }));
       logger.info(`Test data generated successfully.`);
       process.exit(0);
     } catch (err) {
